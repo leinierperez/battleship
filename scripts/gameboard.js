f562cf0976404470b1826d56ab5aeba4cfa40452
Leinier Perez <P_leinier@yahoo.com>
2022-06-13 16:17:05 -0400

    Create tests for placing ship horizontally & verically & make them pass

--- scripts/gameboard.js
+++ scripts/gameboard.js
@@ -1,9 +1,29 @@
+import ship from '../scripts/ship.js';
+
 const gameboard = () => {
   const board = new Array(10).fill(null).map(() => new Array(10).fill(null));
 
   const getBoard = () => board;
 
-  return { getBoard };
+  const placeShip = (x, y, shipName, shipLength, orientation) => {
+    const newShip = ship(shipName, shipLength);
+    if (orientation === 'horizontal') {
+      for (let i = 0; i < newShip.length; i++) {
+        board[x][i + y] = newShip;
+      }
+    } else if (orientation === 'vertical') {
+      for (let i = 0; i < newShip.length; i++) {
+        board[i + x][y] = newShip;
+      }
+    }
+  };
+
+  const isShipAtLocation = (x, y) => {
+    if (board[x][y] === null) return false;
+    else return true;
+  };
+
+  return { getBoard, placeShip, isShipAtLocation };
 };
 
 export default gameboard;
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -6,3 +6,22 @@ test('should initialize gameboard and return it', () => {
     new Array(10).fill(null).map(() => new Array(10).fill(null))
   );
 });
+
+test('should place a ship on the gameboard horizontally', () => {
+  const newGameboard = gameboard();
+  newGameboard.placeShip(2, 1, 'Carrier', 5, 'horizontal');
+  expect(newGameboard.isShipAtLocation(2, 1)).toBe(!null);
+  expect(newGameboard.isShipAtLocation(2, 2)).toBe(!null);
+  expect(newGameboard.isShipAtLocation(2, 3)).toBe(!null);
+  expect(newGameboard.isShipAtLocation(2, 4)).toBe(!null);
+  expect(newGameboard.isShipAtLocation(2, 5)).toBe(!null);
+});
+
+test('should place a ship on the gameboard vertically', () => {
+  const newGameboard = gameboard();
+  newGameboard.placeShip(4, 2, 'Battleship', 4, 'vertical');
+  expect(newGameboard.isShipAtLocation(4, 2)).toBe(!null);
+  expect(newGameboard.isShipAtLocation(5, 2)).toBe(!null);
+  expect(newGameboard.isShipAtLocation(6, 2)).toBe(!null);
+  expect(newGameboard.isShipAtLocation(7, 2)).toBe(!null);
+});
